d45b99320c72c268d47c3486c68ebe2d02645098
Josip Povreslo <josip@field39.com>
2023-09-25 11:46:44 +0200

    Initial Commit

---
+++ wSwitch/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  wSwitch
+//
+//  Created by Josip Povreslo on 25.09.2023..
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ wSwitch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  wSwitch
+//
+//  Created by Josip Povreslo on 25.09.2023..
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
